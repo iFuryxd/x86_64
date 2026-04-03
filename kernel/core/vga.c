eb1c6b77539c21e1564d1b762888691f311ad826
iFuryxd <paduretadrian91@gmail.com>
2026-04-03 10:50:44 -0700

    fixed and improved various things

--- kernel/core/vga.c
+++ kernel/core/vga.c
@@ -1,6 +1,11 @@
 #include <kernel/vga.h>
 
-static uint16_t *vga_buffer = (uint16_t *)0xB8000;
+typedef struct {
+  uint8_t character;
+  uint8_t attribute;
+} __attribute__((packed)) vga_entry_t;
+
+static volatile vga_entry_t* vga = (vga_entry_t*)0xB8000;
 static uint8_t color = 0x0F;
 
 static uint32_t cursor_x = 0;
@@ -13,7 +18,9 @@ static void putchar(char c) {
     return;
   } else {
     uint32_t index = cursor_y * VGA_WIDTH + cursor_x;
-    vga_buffer[index] = (color << 8) | (uint8_t)c;
+    
+    vga[index].character = c;
+    vga[index].attribute = color;
 
     cursor_x++;
   }
@@ -35,19 +42,20 @@ void print(const char *str) {
 }
 
 void print_hex(uint32_t val) {
-  const char *hex = "0123456789ABCDEF";
+    const char *hex = "0123456789ABCDEF";
 
-  print("0x");
-  for (int i = 7; i >= 0; i--) {
-    uint32_t shift = i * 4;
-    uint32_t nibble = (val >> shift) & 0xF;
-    putchar(hex[nibble]);
-  }
+    print("0x");
+
+    for (int i = 0; i < 8; i++) {
+        uint32_t shift = (7 - i) * 4;
+        uint32_t nibble = (val >> shift) & 0xF;
+        putchar(hex[nibble]);
+    }
 }
 
 void clear(void) {
   for (uint32_t i = 0; i < VGA_WIDTH * VGA_HEIGHT; i++) {
-    vga_buffer[i] = (color << 8) | ' ';
+    vga[i].character = ' ';
   }
   cursor_x = 0;
   cursor_y = 0;
