a014daebf6d49ce957293fc51530167c03614ce2
iFuryxd <paduretadrian91@gmail.com>
2026-04-03 10:58:50 -0700

    added more kernel messages and called parse_mmap()

--- kernel/core/kernel.c
+++ kernel/core/kernel.c
@@ -1,7 +1,8 @@
 #include <common/types.h>
 #include <kernel/vga.h>
+#include <kernel/multiboot_info.h>
 void kernel_main(uint32_t magic, uint32_t multiboot_info) {
-  print("KERNEL_EVENT: STARTING KERNEL");
+  print("KERNEL_EVENT: STARTING KERNEL\n");
   if (magic != 0x36D76289) {
     print("KERNEL_EVENT: BAD MAGIC");
     print("\n");
@@ -12,16 +13,14 @@ void kernel_main(uint32_t magic, uint32_t multiboot_info) {
     while (1) {
     }
   }
-  print("\n");
-  print("KERNEL_EVENT: MAGIC CHECK PASSED");
-  print("\n");
+  print("KERNEL_EVENT: MAGIC CHECK PASSED\n");
   print("magic=");
   print_hex(magic);
-  print("\n");
-  print("multiboot=");
+  print("\nmultiboot_info=");
   print_hex(multiboot_info);
-  print("\n");
-  for (;;) {
+  print("\nKERNEL_EVENT: PARSING MULTIBOOT_INFO\n");
+  parse_mmap(multiboot_info);
+  while(1) {
     __asm__ volatile("cli; hlt");
   }
 }
