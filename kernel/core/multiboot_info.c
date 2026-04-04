67195c43d039bd6de1323beb3b01f39d5bc5cda6
iFuryxd <paduretadrian91@gmail.com>
2026-04-04 08:03:06 -0700

    added uint8_t WRITE_TO_VGA flag to parse_mmap and a if statement to check if the flag is set or no, if (WRITE_TO_VGA) {...}

--- kernel/core/multiboot_info.c
+++ kernel/core/multiboot_info.c
@@ -1,7 +1,7 @@
 #include <kernel/multiboot_info.h>
 #include <kernel/vga.h>
 
-void parse_mmap(uint32_t multiboot_info) {
+void parse_mmap(uint32_t multiboot_info, uint8_t WRITE_TO_VGA) {
   multiboot_info_t *mbi = (multiboot_info_t *)multiboot_info;
   multiboot_tag_t *tag = (multiboot_tag_t *)(mbi + 1);
 
@@ -15,16 +15,18 @@ void parse_mmap(uint32_t multiboot_info) {
         (multiboot_mmap_entry_t *)((uint8_t *)mmap + sizeof(multiboot_tag_mmap_t));
 
     while ((uint8_t *)entry < (uint8_t *)mmap + mmap->size) {
-      print("\nregion: ");
-      print_hex((uint32_t)entry->addr);
-      print("\nsize=");
-      print_hex((uint32_t)entry->len);
-      print("\ntype=");
-      print_hex(entry->type);
-      print("\n");
+      if (WRITE_TO_VGA) {
+      vga_write("\nregion: ");
+      vga_write_hex((uint32_t)entry->addr);
+      vga_write("\nsize=");
+      vga_write_hex((uint32_t)entry->len);
+      vga_write("\ntype=");
+      vga_write_hex(entry->type);
+      vga_write("\n");
+      }
       entry = (multiboot_mmap_entry_t *)((uint8_t *)entry + mmap->entry_size);
     }
   } else {
-    print("KERNEL_EVENT: tag->type != 6\n HALTING...");
+    vga_write("KERNEL_EVENT: tag->type != 6\n HALTING...");
   }
 }
