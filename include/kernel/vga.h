c97d5dbba8f81ebbf05ac79a26c9c1dc1d051316
iFuryxd <paduretadrian91@gmail.com>
2026-04-04 07:57:30 -0700

    added vga_color enum, changed print and putchar to vga_write and vga_pchar, added vga_set_color and vga_make_color, changed clear to vga_clear

--- include/kernel/vga.h
+++ include/kernel/vga.h
@@ -6,7 +6,29 @@
 #define VGA_WIDTH 80
 #define VGA_HEIGHT 25
 
-void print(const char *str);
-void print_hex(uint32_t val);
-void clear(void);
+enum vga_color {
+    VGA_COLOR_BLACK = 0,
+    VGA_COLOR_BLUE = 1,
+    VGA_COLOR_GREEN = 2,
+    VGA_COLOR_CYAN = 3,
+    VGA_COLOR_RED = 4,
+    VGA_COLOR_MAGENTA = 5,
+    VGA_COLOR_BROWN = 6,
+    VGA_COLOR_LIGHT_GREY = 7,
+    VGA_COLOR_DARK_GREY = 8,
+    VGA_COLOR_LIGHT_BLUE = 9,
+    VGA_COLOR_LIGHT_GREEN = 10,
+    VGA_COLOR_LIGHT_CYAN = 11,
+    VGA_COLOR_LIGHT_RED = 12,
+    VGA_COLOR_LIGHT_MAGENTA = 13,
+    VGA_COLOR_LIGHT_BROWN = 14,
+    VGA_COLOR_WHITE = 15
+};
+
+void vga_write(const char *str);
+void vga_write_hex(uint32_t val);
+void vga_set_color(uint8_t color);
+uint8_t vga_make_color(uint8_t fg, uint8_t bg);
+void vga_clear(void);
+
 #endif
