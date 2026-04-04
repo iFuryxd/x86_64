0dd2d2817faea14dfe4304b92526133c41582871
iFuryxd <paduretadrian91@gmail.com>
2026-04-03 17:50:11 -0700

    added structures for multiboot parsing

--- include/kernel/multiboot_info.h
+++ include/kernel/multiboot_info.h
@@ -1,7 +1,32 @@
 #ifndef MULTIBOOT_INFO_H
 #define MULTIBOOT_INFO_H
 
-typedef struct multiboot_info_t;
-typedef struct multiboot_i;
+#include <common/types.h>
+
+typedef struct {
+  uint32_t size;
+  uint32_t reserved;
+} multiboot_info_t;
+
+typedef struct {
+  uint32_t type;
+  uint32_t size;
+} multiboot_tag_t;
+
+typedef struct {
+  uint32_t type;
+  uint32_t size;
+  uint32_t entry_size;
+  uint32_t entry_version;
+} multiboot_tag_mmap_t;
+
+typedef struct {
+  uint64_t addr;
+  uint64_t len;
+  uint32_t type;
+  uint32_t zero;
+} multiboot_mmap_entry_t;
+
+void parse_mmap(uint32_t multiboot_info);
 
 #endif
